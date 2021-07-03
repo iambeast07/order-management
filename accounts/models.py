b19f1839b932d9c1b34f9226b1d6d038b67cb9a5
iambeast07 <vimal262000@gmail.com>
2021-07-02 21:03:51 -0700

    all files

---
+++ accounts/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(
+        User, blank=True, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    profile_pic = models.ImageField(
+        default="profile1.png", null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return str(self.user or self.name)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    note = models.CharField(max_length=1000, null=True)
+
+    def __str__(self):
+        return str(self.product.name)
+    
+    @staticmethod
+    def total_orders():
+        return Order.objects.count()
+    
+    @staticmethod
+    def orders_delivered():
+        return Order.objects.filter(status='Delivered').count()
+
+    @staticmethod
+    def orders_pending():
+        return Order.objects.filter(status='Pending').count() 
